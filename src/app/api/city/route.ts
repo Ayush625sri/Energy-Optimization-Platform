916a357f8cdedea47260412ff272700525cf659d
Ayush Srivastava <ayush625sri@gmail.com>
2024-10-25 14:39:14 +0530

    mongodb integrated

---
+++ src/app/api/city/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import { connectToDatabase } from '@/app/db/connect';
+import CityData from '@/app/db/models/CityData';
+
+// GET Request: Fetch all city data
+export async function GET() {
+  try {
+    // Ensure the database is connected
+    await connectToDatabase();
+
+    // Fetch city data from the CityData collection
+    const cities = await CityData.find({});
+    console.log(cities)
+    // Return the fetched data as JSON
+    return NextResponse.json(cities, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching city data:', error);
+    return NextResponse.json({ error: 'Failed to fetch city data' }, { status: 500 });
+
+  }
+}
